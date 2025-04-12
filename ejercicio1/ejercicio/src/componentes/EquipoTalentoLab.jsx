1b7503cd0255d957646dbbe42dc451988c968b20
ErwinAdriel <adrielquispe87@gmail.com>
2025-04-12 20:18:41 -0300

    Estructura de EquipoTalentoLab

--- ejercicio1/ejercicio/src/componentes/EquipoTalentoLab.jsx
+++ ejercicio1/ejercicio/src/componentes/EquipoTalentoLab.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import Miembro from "./Miembro";
 
 export default function EquipoTalentoLab(){
+    const equipo = [
+        {nombre: "Silvia", rol: "Product Owner"},
+        {nombre: "Luis", rol: "Diseñador UX/UI"},
+        {nombre: "Matias", rol: "Desarrollador"},
+        {nombre: "Sabrina", rol: "Desarrolladora"}
+    ];
     
+    /*
+    const nombre = "Silvia";
+    const rol = "Product Owner";
+    */
+    return(
+        <ul>
+            {
+                equipo.map((eq, index) => (
+                    <li key={index}>
+                        <Miembro nombre={eq.nombre} rol={eq.rol}/>
+                    </li>
+                ))
+            }
+        </ul>
+    )
 }
--- ejercicio1/ejercicio/src/componentes/Miembro.jsx
+++ ejercicio1/ejercicio/src/componentes/Miembro.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function Miembro(){
-    
+export default function Miembro({nombre, rol}){
+    return(
+        <div>
+            <p>{nombre}</p>
+            <p>{rol}</p>
+        </div>
+    )
+        
 }
