3b3a41bda5800e3ea7ac811e9b7b78494c78e3f0
ErwinAdriel <adrielquispe87@gmail.com>
2025-05-14 21:08:53 -0300

    Estado de carga y error

---
+++ carrito/src/componentes/Error404.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Error(){
+  return(
+    <main class="grid min-h-full place-items-center bg-white px-6 py-24 lg:px-8">
+      <div class="text-center">
+        <p class="font-semibold text-8xl text-slate-900">404</p>
+        <h1 class="mt-4 text-5xl font-semibold tracing-tight text-balance text-gray-900 sm:text-5xl">Página no encontrada</h1>
+        <p class="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/4">Lo sentimos, no pudimos encontrar la página.</p>
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+          <a href="#" class="rounded-md bg-slate-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-slate-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Volver</a>
+          <a href="#" class="text-sm font-semibold text-gray-900">Contacto soporte <span aria-hidden="true">&rarr;</span></a>
+        </div>
+      </div>
+    </main>
+  )
+}
--- carrito/src/componentes/ProductList.jsx
+++ carrito/src/componentes/ProductList.jsx
@@ -1,24 +1,49 @@
 import React, { useEffect, useState } from "react";
 import Producto from "./Producto";
+import loading from "../img/loading.gif";
+import Error from "./Error404";
 
 export default function ProductList({addToCart}){
 
     const [products, setProducts] = useState([]);
+    const [carga, setCarga] = useState(true);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         fetch('/data/data.json')
             .then((respuesta) => respuesta.json())
-            .then((datos) => setProducts(datos))
-            .catch((error) => console.error("Error: ", error));
+            .then((datos) => {
+                setTimeout(() => {
+                    setProducts(datos)
+                    setCarga(false)    
+                }, 2000)
+            })
+            .catch((error) => {
+                console.error("Error: ", error)
+                setTimeout(() => {
+                    setCarga(false)
+                    setError(true)    
+                }, 2000);
+            });
     }, []);
 
+    if(error){
+        return <Error />
+    }
+
     return(
-        <div class="grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-10">
-            {
-                products.map((product) => (
-                    <Producto key={product.id} product={product} addToCart={addToCart}/>
-                ))
-            }
-        </div>
+        <>
+        {
+            carga ? <img class="mx-auto" src={loading} alt="loading" /> :
+        
+            <div class="grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-10">
+                {
+                    products.map((product) => (
+                        <Producto key={product.id} product={product} addToCart={addToCart}/>
+                    ))
+                }
+            </div>
+        }
+        </>
     )
 }
