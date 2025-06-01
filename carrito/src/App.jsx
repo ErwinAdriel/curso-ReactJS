98f294c4d9fa86a741d7cf3e31ece61092a17a3f
ErwinAdriel <adrielquispe87@gmail.com>
2025-06-01 14:34:04 -0300

    Uso de UseContext en App

--- carrito/src/App.jsx
+++ carrito/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import "./App.css";
 import Home from "./layout/Home";
 import Footer from "./componentes/Footer";
@@ -11,163 +11,76 @@ import GaleriadeProductos from "./layout/GaleriadeProductos";
 import Admin from "./layout/Admin";
 import Login from "./layout/Login";
 import RutaProtegida from "./auth/RutasProtegidas";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { CartContext } from "./context/CartContext";
 
 function App() {
-  const [cart, setCart] = useState([]);
-  const [products, setProducts] = useState([]);
-  const [carga, setCarga] = useState(true);
-  const [vacio, setVacio] = useState(true);
-  const [error, setError] = useState(false);
-  const [precio, setPrecio] = useState();
-  const [isAuthenticated, setIsAuth] = useState(false);
-
-  useEffect(() => {
-    fetch("/data/data.json")
-      .then((respuesta) => respuesta.json())
-      .then((datos) => {
-        setTimeout(() => {
-          setProducts(datos);
-          setCarga(false);
-        }, 2000);
-      })
-      .catch((error) => {
-        console.error("Error: ", error);
-        setTimeout(() => {
-          setCarga(false);
-          setError(true);
-        }, 2000);
-      });
-  }, []);
-
-  //console.log(cart);
-
-  function handleAddToCart(product) {
-    setPrecio(product.price);
-    const productExist = cart.find((item) => item.id === product.id);
-
-    if (productExist) {
-      /*if(productExist.id == product.id){
-        setCart([...cart, {...product}])
-      }*/
-      //Si el producto esta en el carrito
-      setCart(
-        cart.map((item) => {
-          if (item.id === product.id) {
-            if (item.cantidad < product.stock) {
-              return { ...item, cantidad: item.cantidad + 1 };
-            }
-
-            return item;
-          } else {
-            return item;
-          }
-        })
-      );
-    } else {
-      setCart([...cart, { ...product, cantidad: 1 }]);
-      setVacio(false);
-    }
-  }
-
-  function eliminarCant(product) {
-    setCart((prevCart) => {
-      return prevCart
-        .map((item) => {
-          if (item.id === product.id) {
-            if (item.cantidad > 1) {
-              return { ...item, cantidad: item.cantidad - 1 };
-            } else {
-              if (prevCart.length < 2) {
-                setVacio(true);
-              }
-              return null;
-            }
-          } else {
-            return item;
-          }
-        })
-        .filter((item) => item != null);
-    });
-  }
-
-  function agregarCant(product) {
-    setCart((afterCart) => {
-      return afterCart
-        .map((item) => {
-          if (item.id === product.id) {
-            if (item.cantidad < product.stock) {
-              return {
-                ...item,
-                cantidad: item.cantidad + 1,
-                price: precio + item.price,
-              };
-            } else {
-              return item;
-            }
-          } else {
-            return item;
-          }
-        })
-        .filter((item) => item === item);
-    });
-  }
-
-  function vaciarCart() {
-    setCart([]);
-    setVacio(true);
-    console.log("Carrito vacio");
-  }
+  const {
+    cart,
+    products,
+    carga,
+    vacio,
+    error,
+    handleAddToCart,
+    eliminarCant,
+    agregarCant,
+    vaciarCart,
+    isAuthenticated
+  } = useContext(CartContext);
 
   return (
     <>
-      <Header
-        cartItems={cart}
-        vaciarItems={vaciarCart}
-        eliminarItem={eliminarCant}
-        vacio={vacio}
-        agregarItem={agregarCant}
-      />
-      <Routes>
-        <Route path="*" element={<Error />} />
-        <Route
-          path="/"
-          element={
-            <Home
-              products={products}
-              cargando={carga}
-              cart={cart}
-              handleAddToCart={handleAddToCart}
-            />
-          }
+      <Router>
+        <Header
+          cartItems={cart}
+          vaciarItems={vaciarCart}
+          eliminarItem={eliminarCant}
+          vacio={vacio}
+          agregarItem={agregarCant}
         />
-        <Route
-          path="/productos"
-          element={<GaleriadeProductos products={products} cargando={carga} />}
-        />
-        <Route
-          path="/productos/:id"
-          element={
-            <DetallesProductos
-              products={products}
-              addToCart={handleAddToCart}
-            />
-          }
-        />
-        <Route path="/nosotros" element={<Nosotros />} />
-        <Route path="/contacto" element={<Contacto />} />
-        <Route
-          path="/admin"
-          element={
-            <RutaProtegida isAuthenticated={isAuthenticated}>
-              {" "}
-              <Admin />{" "}
-            </RutaProtegida>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-      <Footer />
+        <Routes>
+          <Route path="*" element={<Error />} />
+          <Route
+            path="/"
+            element={
+              <Home
+                products={products}
+                cargando={carga}
+                cart={cart}
+                handleAddToCart={handleAddToCart}
+              />
+            }
+          />
+          <Route
+            path="/productos"
+            element={
+              <GaleriadeProductos products={products} cargando={carga} />
+            }
+          />
+          <Route
+            path="/productos/:id"
+            element={
+              <DetallesProductos
+                products={products}
+                addToCart={handleAddToCart}
+              />
+            }
+          />
+          <Route path="/nosotros" element={<Nosotros />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route
+            path="/admin"
+            element={
+              <RutaProtegida isAuthenticated={isAuthenticated}>
+                {" "}
+                <Admin />{" "}
+              </RutaProtegida>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 }
---
+++ carrito/src/context/CartContext.jsx
@@ -0,0 +1,129 @@
+import { createContext, useState, useEffect } from "react";
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [carga, setCarga] = useState(true);
+  const [vacio, setVacio] = useState(true);
+  const [error, setError] = useState(false);
+  const [precio, setPrecio] = useState();
+  const [isAuthenticated, setIsAuth] = useState(false);
+
+  useEffect(() => {
+    fetch("/data/data.json")
+      .then((respuesta) => respuesta.json())
+      .then((datos) => {
+        setTimeout(() => {
+          setProducts(datos);
+          setCarga(false);
+        }, 2000);
+      })
+      .catch((error) => {
+        console.error("Error: ", error);
+        setTimeout(() => {
+          setCarga(false);
+          setError(true);
+        }, 2000);
+      });
+  }, []);
+
+  //console.log(cart);
+
+  function handleAddToCart(product) {
+    setPrecio(product.price);
+    const productExist = cart.find((item) => item.id === product.id);
+
+    if (productExist) {
+      /*if(productExist.id == product.id){
+            setCart([...cart, {...product}])
+        }*/
+      //Si el producto esta en el carrito
+      setCart(
+        cart.map((item) => {
+          if (item.id === product.id) {
+            if (item.cantidad < product.stock) {
+              return { ...item, cantidad: item.cantidad + 1 };
+            }
+
+            return item;
+          } else {
+            return item;
+          }
+        })
+      );
+    } else {
+      setCart([...cart, { ...product, cantidad: 1 }]);
+      setVacio(false);
+    }
+  }
+
+  function eliminarCant(product) {
+    setCart((prevCart) => {
+      return prevCart
+        .map((item) => {
+          if (item.id === product.id) {
+            if (item.cantidad > 1) {
+              return { ...item, cantidad: item.cantidad - 1 };
+            } else {
+              if (prevCart.length < 2) {
+                setVacio(true);
+              }
+              return null;
+            }
+          } else {
+            return item;
+          }
+        })
+        .filter((item) => item != null);
+    });
+  }
+
+  function agregarCant(product) {
+    setCart((afterCart) => {
+      return afterCart
+        .map((item) => {
+          if (item.id === product.id) {
+            if (item.cantidad < product.stock) {
+              return {
+                ...item,
+                cantidad: item.cantidad + 1,
+                price: precio + item.price,
+              };
+            } else {
+              return item;
+            }
+          } else {
+            return item;
+          }
+        })
+        .filter((item) => item === item);
+    });
+  }
+
+  function vaciarCart() {
+    setCart([]);
+    setVacio(true);
+    console.log("Carrito vacio");
+  }
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        products,
+        carga,
+        vacio,
+        error,
+        handleAddToCart,
+        eliminarCant,
+        agregarCant,
+        vaciarCart,
+        isAuthenticated
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
